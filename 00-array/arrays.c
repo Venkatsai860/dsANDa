afba7713b78c2855badc12eeb8ca9a0a69afb97b
the-appretice <theappretice@ta.com>
2021-03-28 19:02:43 +0000

    figured reverse. now rotating

--- 00-array/arrays.c
+++ 00-array/arrays.c
@@ -20,11 +20,26 @@ void printstrarr(char **str, int size){
 }
 
 
+void reverse_arr(int *arr, int size) {
+	for (int i =0; i < size / 2; i++){
+		int temp = arr[i];
+		arr[i] = arr[size-i-1];
+		arr[size - i-1] = temp;
+	}
+}
+
+
 int main(){
-	int intarr1[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	
+	int intarr1[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};	// can manually do intarr1[size]
+	int intarr1size = sizeof(intarr1)/sizeof(int);
+
 	char *strarr1[3] = {"hi", "hello", "bye"};
 
-	printintarr(intarr1, sizeof(intarr1)/sizeof(int));
+	//PRINT
+	printintarr(intarr1, intarr1size); 
 	printstrarr(strarr1, sizeof(strarr1)/sizeof(char *));
+
+	//REVERSE
+	reverse_arr(intarr1, intarr1size);
+	printintarr(intarr1, intarr1size);
 }
